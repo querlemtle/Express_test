a248ad90e2fd4b1a71601809c7a89e2de81faee6
querlemtle <citta120120@gmail.com>
2022-04-24 19:46:34 +0800

    format to follow js standard style

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const app = express();
-const exphbs = require('express-handlebars');
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
 
-const port = 3000;
+const port = 3000
 
 // Set up the template engine
-app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 
 // Set static files
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Set routes
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 app.get('/:nav_item', (req, res) => {
-  // req.params.nav_item 取得名稱，將首字轉為大寫再接回剩下字串
-  const title = req.params.nav_item.charAt(0).toUpperCase() + req.params.nav_item.slice(1);
-  res.render('others', { title });
-});
+  // Get title from req.params.nav_item
+  // Capitalize and concatenate the rest string
+  const title = req.params.nav_item.charAt(0).toUpperCase() + req.params.nav_item.slice(1)
+  res.render('others', { title })
+})
 
 // Start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`);
-});
+  console.log(`Express is running on http://localhost:${port}`)
+})
